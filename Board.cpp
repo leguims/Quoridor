ab1d18afda67521e3dc7a1a4e5cf1285d692ce71
leguims <leguims@free.fr>
2018-06-25 20:29:52 +0200

    Add some move list + refactoring board.

--- Board.cpp
+++ Board.cpp
@@ -160,13 +160,25 @@ bool AStarBoard::findPath(const WallPosition& wall, const BoardPosition& current
 void AStarBoard::add(const WallPosition &wall)
 {
     for (const auto v : position(wall))
-        generator_.addCollision(v);
+    {
+        try
+        {
+            generator_.addCollision(v);
+        }
+        catch (const std::out_of_range &) {}
+    }
 }
 
 void AStarBoard::remove(const WallPosition &wall)
 {
     for (const auto v : position(wall))
-        generator_.removeCollision(v);
+    {
+        try
+        {
+            generator_.removeCollision(v);
+        }
+        catch (const std::out_of_range &) {}
+    }
 }
 
 AStar::Vec2i AStarBoard::position(const BoardPosition &bp)
@@ -179,15 +191,17 @@ std::vector<AStar::Vec2i> AStarBoard::position(const WallPosition &wp)
     std::vector<AStar::Vec2i> list;
     if (wp.direction() == WallPosition::Direction::horizontal)
     {
-        list.push_back({ 2 * wp.x() - 2, 2 * wp.y() - 1 });
+        list.push_back({ 2 * wp.x() - 2 + 0, 2 * wp.y() - 1 });
         list.push_back({ 2 * wp.x() - 2 + 1, 2 * wp.y() - 1 });
         list.push_back({ 2 * wp.x() - 2 + 2, 2 * wp.y() - 1 });
+        list.push_back({ 2 * wp.x() - 2 + 3, 2 * wp.y() - 1 });
     }
     else
     {
-        list.push_back({ 2 * wp.x() - 1, 2 * wp.y() - 2 });
+        list.push_back({ 2 * wp.x() - 1, 2 * wp.y() - 2 + 0 });
         list.push_back({ 2 * wp.x() - 1, 2 * wp.y() - 2 + 1 });
         list.push_back({ 2 * wp.x() - 1, 2 * wp.y() - 2 + 2 });
+        list.push_back({ 2 * wp.x() - 1, 2 * wp.y() - 2 + 3 });
     }
     return list;
 }
--- Player.cpp
+++ Player.cpp
@@ -21,27 +21,40 @@ Move Player::getNextMove(const unsigned int &round, const Board &board, const st
     //    "e6", "d4",
     //    "c3h", "a3h",
     //    "e7", "e7h",
-    //    "d6v", "d4v",
-    //    "e8", "d3",
-    //    "e9", "d2"
+    //    "d6v", "d4v"
     //};
 
-    // Meet on center and create closed square around player and then jump each other.
-    // Wall "c4v" is ILLEGAL MOVE !
     std::vector<std::string> move_list{
         "e2", "e8",
         "e3", "e7",
         "e4", "e6",
-        "d3h", "e5",
-        "e4v", "d5h",
-        "c4v", "d4",
-        "d5", "e5",
-        "e4", "d5",
-        "d4", "e4",
-        "e5", "d5",
-        "d4", "e5",
-        "e4", "d4",
+        "e3h", "g6v",
+        "e5", "e4",
+        "e6", "d4",
+        "c3h", "a3h",
+        "e7", "e7h",
+        "d6v", "d4v",
+        "e8", "d3", // e8 illegal because of "e7h" !
+        "e9", "d2"
     };
+
+    // Meet on center and create closed square around player and then jump each other.
+    // Wall "c4v" is ILLEGAL MOVE !
+    //std::vector<std::string> move_list{
+    //    "e2", "e8",
+    //    "e3", "e7",
+    //    "e4", "e6",
+    //    "d3h", "e5",
+    //    "e4v", "d5h",
+    //    "c4v", "d4",
+    //    "d5", "e5",
+    //    "e4", "d5",
+    //    "d4", "e4",
+    //    "e5", "d5",
+    //    "d4", "e5",
+    //    "e4", "d4",
+    //};
+
     //std::vector<std::string> move_list{
     //    "e2", "e8",
     //    "e3", "e7",
@@ -54,6 +67,7 @@ Move Player::getNextMove(const unsigned int &round, const Board &board, const st
     //    "e8", "e1",
     //    "e9", "f1"
     //};
+
     if ( (2*round) > move_list.size())
         return Move();
     else
