637734fbe64139290754692ef7e0e1ecf5c54651
leguims <leguims@free.fr>
2018-07-14 12:45:02 +0200

    Add draft in main() to use PDCurses.

--- Quoridor.cpp
+++ Quoridor.cpp
@@ -16,8 +16,164 @@ void test_Replay();
 void test_Game_IA_linear();
 void test_Game_IA_random();
 
+
+#include "PDCurses/curses.h"
+
+static int next_j(int j)
+{
+    if (j == 0)
+        j = 4;
+    else
+        --j;
+
+    if (has_colors())
+    {
+        int z = rand() % 3;
+        chtype color = COLOR_PAIR(z);
+
+        if (z)
+            color |= A_BOLD;
+
+        attrset(color);
+    }
+
+    return j;
+}
+
+void main_pdcurses_rain()
+{
+    int x, y, j, r, c, seed;
+    static int xpos[5], ypos[5];
+
+    initscr();                    /* Start curses mode */
+    seed = (int)time((time_t *)0);
+    srand(seed);
+
+    if (has_colors())
+    {
+        short bg = COLOR_BLACK;
+
+        start_color();
+        init_pair(1, COLOR_BLUE, bg);
+        init_pair(2, COLOR_CYAN, bg);
+    }
+
+    nl();
+    noecho();
+    curs_set(0);
+    timeout(0);
+    keypad(stdscr, TRUE);
+
+    r = LINES - 4;
+    c = COLS - 4;
+
+    for (j = 5; --j >= 0;)
+    {
+        xpos[j] = rand() % c + 2;
+        ypos[j] = rand() % r + 2;
+    }
+
+    for (j = 0;;)
+    {
+        x = rand() % c + 2;
+        y = rand() % r + 2;
+
+        mvaddch(y, x, '.');
+
+        mvaddch(ypos[j], xpos[j], 'o');
+
+        j = next_j(j);
+        mvaddch(ypos[j], xpos[j], 'O');
+
+        j = next_j(j);
+        mvaddch(ypos[j] - 1, xpos[j], '-');
+        mvaddstr(ypos[j], xpos[j] - 1, "|.|");
+        mvaddch(ypos[j] + 1, xpos[j], '-');
+
+        j = next_j(j);
+        mvaddch(ypos[j] - 2, xpos[j], '-');
+        mvaddstr(ypos[j] - 1, xpos[j] - 1, "/ \\");
+        mvaddstr(ypos[j], xpos[j] - 2, "| O |");
+        mvaddstr(ypos[j] + 1, xpos[j] - 1, "\\ /");
+        mvaddch(ypos[j] + 2, xpos[j], '-');
+
+        j = next_j(j);
+        mvaddch(ypos[j] - 2, xpos[j], ' ');
+        mvaddstr(ypos[j] - 1, xpos[j] - 1, "   ");
+        mvaddstr(ypos[j], xpos[j] - 2, "     ");
+        mvaddstr(ypos[j] + 1, xpos[j] - 1, "   ");
+        mvaddch(ypos[j] + 2, xpos[j], ' ');
+
+        xpos[j] = x;
+        ypos[j] = y;
+
+        switch (getch())
+        {
+        case 'q':
+        case 'Q':
+            curs_set(1);
+            endwin();
+            return;
+        case 's':
+            nodelay(stdscr, FALSE);
+            break;
+        case ' ':
+            nodelay(stdscr, TRUE);
+#ifdef KEY_RESIZE
+            break;
+        case KEY_RESIZE:
+# ifdef PDCURSES
+            resize_term(0, 0);
+            erase();
+# endif
+            r = LINES - 4;
+            c = COLS - 4;
+#endif
+        }
+        napms(50);
+    }
+}
+
+void main_pdcurses_helloworld()
+{
+    initscr();                    /* Start curses mode */
+    printw("Hello World !!!");    /* Print Hello World */
+    refresh();                    /* Print it on to the real screen */
+    getch();                      /* Wait for user input */
+    endwin();                     /* End curses mode */
+}
+
+void main_pdcurses_board()
+{
+    initscr();                  /* Start curses mode */
+    raw();				        /* Line buffering disabled	*/
+    keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
+    noecho();			        /* Don't echo() while we do getch */
+    //halfdelay(5);             /* Set timeout with getch() */
+
+    printw("Hello World !!!");    /* Print Hello World */
+    refresh();                    /* Print it on to the real screen */
+
+    getch();                      /* Wait for user input */
+    endwin();                     /* End curses mode */
+}
+
+void main_pdcurses()
+{
+    // Source : http://tldp.org/HOWTO/NCURSES-Programming-HOWTO/
+    // Source : https://pdcurses.sourceforge.io/doc/PDCurses.txt
+    // Source : https://www.projectpluto.com/win32a.htm
+
+    //main_pdcurses_rain();
+    //main_pdcurses_helloworld();
+    main_pdcurses_board();
+}
+
+
 int main()
 {
+    main_pdcurses();
+
     test_BoardPosition();
     test_Board();
     test_Replay();
